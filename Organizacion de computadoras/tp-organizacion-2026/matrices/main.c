2d1485bf733cebb100989e0b3ea97cceb076ecaa
jedihilario <hyanezbertola@gmail.com>
2026-06-16 01:23:12 -0300

    fix: Fix multiplication registers and add comments

--- Organizacion de computadoras/tp-organizacion-2026/matrices/main.c
+++ Organizacion de computadoras/tp-organizacion-2026/matrices/main.c
@@ -4,13 +4,16 @@
 extern int add_matrices (int* m, int* n, int* res, short s);
 
 int main (void) {
+    // Declare to 3x3 matrices, result matrix and size variable
     int a[3][3] = { { 10, 60, 7 }, { 3, 4, 6 }, { 2, 2, 2 } };
     int b[3][3] = { { 1, 7, 5 }, { 3, 4, 8 }, { 1, 1, 1 } };
     int res[3][3];
     short s = 3;
 
+    // Call addition subroutine
     add_matrices(&a[0][0], &b[0][0], &res[0][0], s);
 
+    // Iterate over result matrix elements and print them to stdout
     for (int i = 0; i < s; i++) {
         printf("[ ");
         for (int j = 0; j < s; j++) {
--- Organizacion de computadoras/tp-organizacion-2026/matrices/sum.asm
+++ Organizacion de computadoras/tp-organizacion-2026/matrices/sum.asm
@@ -1,26 +1,49 @@
+; Export definition
 section .text
     global add_matrices
 
+; Addition subroutine
 add_matrices:
+    ; Prólogo
+    push ebp
+    mov ebp, esp
+
+    ; Setup registers and fetch parameters
     xor eax, eax
-    mov esi, [esp + 4]
-    mov edi, [esp + 8]
-    mov ax, [esp + 16]
+    mov esi, [esp + 8]
+    mov edi, [esp + 12]
+    mov ax, [esp + 20]
+
+    ; Multiply ax * ax
     mul ax
-    mov ecx, eax
+
+    ; Move the upper half of result and shift to match
+    mov cx, dx
+    shl ecx, 16
+    ; Move lower half of the result
+    mov cx, ax
+
+    ; Reset eax to 0 to use as accumulator
     xor eax, eax
 
-    mov edx, [esp + 12]
+    ; Fetch result matrix address (3rd parameter)
+    mov edx, [esp + 16]
 
+    ; Matrix sum subroutine
     .sum:
+        ; Add correspondig values in eax
         add eax, [esi]
         add eax, [edi]
+        ; Move the sum into the result matrix position
         mov [edx], eax
+        ; Increment pointer register to next address
         add esi, 4
         add edi, 4
         add edx, 4
+        ; Reset eax value to 0 for next sum
         xor eax, eax
-
     loop .sum
 
+    ; Epílogo
+    pop ebp
     ret
